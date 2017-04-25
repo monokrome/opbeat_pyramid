2dfe0b180e31026e30e8a0ba6a59310e1fbad289
Bailey Stoner <bailey@patreon.com>
2017-04-24 17:17:08 -0700

    Use strings to allow setup.py to import this.
    This should never import depencencies, or else setup.py will fail when
    getting the version because the dependencies don't exist yet.

--- opbeat_pyramid/__init__.py
+++ opbeat_pyramid/__init__.py
@@ -1,20 +1,16 @@
 __VERSION__ = '1.0.8'
 
 
-import pyramid.tweens
-
-
 def includeme(config, module_name='opbeat_pyramid'):
     """ Extensibility function for using this module with any Pyramid app. """
 
     config.scan(module_name)
 
+    # TODO: Is there a decorator for this?
     config.add_tween(
         'opbeat_pyramid.subscribers.opbeat_tween_factory',
         over=[
-            pyramid.tweens.EXCVIEW,
-            # if pyramid_tm is in the pipeline we want to track errors caused
-            # by commit/abort so we try to place ourselves over it
+            'pyramid.tweens.EXCVIEW',
             'pyramid_tm.tm_tween_factory',
         ]
     )
